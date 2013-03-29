cc07f2bc44ca9ec174a580f17ba2bb96e413a19f
Đula Nađ <dula.nad@gmail.com>
2013-03-29 17:50:17 +0100

    Added the message encoder and defined several test messages for the acoustic communication with the diver.

--- caddy_usbl/src/MsgEncoder.cpp
+++ caddy_usbl/src/MsgEncoder.cpp
@@ -40,32 +40,75 @@
 #include <bitset>
 #include <iostream>
 #include <boost/integer/integer_mask.hpp>
+#include <cassert>
 
-template <uint8_t type, size_t depthSize, size_t latlonSize, size_t msgSize>
+#define ADD_DIVER_MESSAGE(NAME, CODE, DEPTHSIZE, \
+	LATLONSIZE, MSGSIZE) \
+	struct NAME\
+	{\
+	  enum{type=CODE};\
+		enum{depthSize=DEPTHSIZE,latlonSize=LATLONSIZE,msgSize=MSGSIZE};\
+	};\
+
+
 struct DiverMsg
 {
+	ADD_DIVER_MESSAGE(PositionInit,1,0,22,0);
+	ADD_DIVER_MESSAGE(Position,2,7,18,1);
+	ADD_DIVER_MESSAGE(PositionMsg,3,7,7,23);
+
+	//\todo Add automatic extraction of lat-lon data from double values
+	template <class msg>
 	uint64_t pack()
 	{
-		uint64_t result(type);
-		result <<= depthSize;
-		result |= depth & boost::low_bits_mask_t<depthSize>::sig_bits;
-		result <<= latlonSize;
-		result |= lat & boost::low_bits_mask_t<latlonSize>::sig_bits;
-		result <<= latlonSize;
-		result |= lon & boost::low_bits_mask_t<latlonSize>::sig_bits;
-		result <<= msgSize;
-		result |= payload & boost::low_bits_mask_t<msgSize>::sig_bits;
-		return result;
+		fullmsg = msg::type;
+		fullmsg <<= msg::depthSize;
+		fullmsg |= depth & boost::low_bits_mask_t<msg::depthSize>::sig_bits;
+		fullmsg <<= msg::latlonSize;
+		fullmsg |= lat & boost::low_bits_mask_t<msg::latlonSize>::sig_bits;
+		fullmsg <<= msg::latlonSize;
+		fullmsg |= lon & boost::low_bits_mask_t<msg::latlonSize>::sig_bits;
+		fullmsg <<= msg::msgSize;
+		fullmsg |= this->msg & boost::low_bits_mask_t<msg::msgSize>::sig_bits;
+		return fullmsg;
 	}
 
-	uint64_t depth, lat, lon, payload;
+	static inline uint8_t testType(uint64_t data, size_t msgSize = 48)
+	{
+		return (data >> (msgSize - 4)) & 0xF;
+	}
+
+	template <class msg>
+	bool unpack(uint64_t data)
+	{
+		fullmsg=data;
+		this->msg = data & boost::low_bits_mask_t<msg::msgSize>::sig_bits;
+		data >>= msg::msgSize;
+		lon = data & boost::low_bits_mask_t<msg::latlonSize>::sig_bits;
+		data >>= msg::latlonSize;
+		lat = data & boost::low_bits_mask_t<msg::latlonSize>::sig_bits;
+		data >>= msg::latlonSize;
+		depth = data & boost::low_bits_mask_t<msg::depthSize>::sig_bits;
+		data >>= msg::depthSize;
+
+		assert(((data & 0x0F) == msg::type) &&
+				"DiverMsg desired unpack type and received data type do not match.");
+	}
+
+	template <size_t precission>
+	std::pair<int,int> latlonToBits(double lat, double lon){return std::pair<int,int>();};
+
+	double lat, lon, depth;
+	uint64_t fullmsg;
+	uint8_t depth;
+	int lat,lon;
+	uint64_t msg;
+	//uint8_t noKML, def, checksum;
+	//int kmlX, kmlY;
 };
 
-template <size_t precission>
-std::pair<int,int> latlonToBits(double lat, double lon);
-
 template <>
-inline std::pair<int,int> latlonToBits<22>(double lat, double lon)
+inline std::pair<int,int> DiverMsg::latlonToBits<22>(double lat, double lon)
 {
 	double min = (lon - int(lon))*60;
 	int clon = int((int(lon)+180)*10000 + min*100);
@@ -77,13 +120,13 @@ inline std::pair<int,int> latlonToBits<22>(double lat, double lon)
 }
 
 template <>
-inline std::pair<int,int> latlonToBits<18>(double lat, double lon)
+inline std::pair<int,int> DiverMsg::latlonToBits<18>(double lat, double lon)
 {
 	return std::pair<int,int>(int((lat - int(lat))*600000)%100000,int((lon - int(lon))*600000)%100000);
 }
 
 template <>
-std::pair<int,int> latlonToBits<14>(double lat, double lon)
+std::pair<int,int> DiverMsg::latlonToBits<14>(double lat, double lon)
 {
 	double min = (lon - int(lon))*60;
 	int clon = int((min - int(min))*10000);
@@ -95,7 +138,7 @@ std::pair<int,int> latlonToBits<14>(double lat, double lon)
 }
 
 template <>
-inline std::pair<int,int> latlonToBits<7>(double lat, double lon)
+inline std::pair<int,int> DiverMsg::latlonToBits<7>(double lat, double lon)
 {
 	std::pair<int,int> val = latlonToBits<14>(lat,lon);
 	return std::pair<int,int>(val.first%100,val.second%100);
@@ -108,13 +151,13 @@ int main(int argc, char* argv[])
 	double depth = 5;
 	double lat=45.769216667, lon=16.101851667;
 
-	DiverMsg<3,0,22,0> msg;
+	DiverMsg msg;
 
 	msg.depth = depth*2;
 
 	std::cout<<"Lat min:"<<(lat - int(lat))*60<<", lon min:"<<(lon - int(lon))*60<<std::endl;
 
-	std::pair<int,int> latlon = latlonToBits<22>(lat,lon);
+	std::pair<int,int> latlon = latlonToBits<7>(lat,lon);
 	msg.lat = latlon.first;
 	msg.lon = latlon.second;
 
@@ -124,13 +167,19 @@ int main(int argc, char* argv[])
 	std::bitset<4> type_enc(3);
 	int msgV=0;
 	char* p=reinterpret_cast<char*>(&msgV);
-	p[0] = 1;
-	p[1] = 2;
-	p[2] = 3;
-	msg.payload = msgV;
+	p[0] = 48;
+	p[1] = 49;
+	p[2] = 50;
+	msg.msg = msgV;
 
-	std::bitset<48> result(msg.pack());
+	std::bitset<48> result(msg.pack<DiverMsg::PositionMsg>());
 	std::cout<<"Result:"<<result<<std::endl;
+
+	DiverMsg msg2;
+	msg2.unpack<DiverMsg::PositionMsg>(msg.pack<DiverMsg::PositionMsg>());
+	std::cout<<"Lat dec:"<<msg2.lat<<", lon dec:"<<msg2.lon<<std::endl;
+	const char* pt=reinterpret_cast<const char*>(&msg2.msg);
+	std::cout<<"Received message:"<<pt[0]<<pt[1]<<pt[2]<<std::endl;
 
 	return 0;
 }
