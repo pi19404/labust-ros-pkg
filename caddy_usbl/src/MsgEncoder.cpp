31aa490d44335b65926a6a1bf0d651e30948145b
Đula Nađ <dula.nad@gmail.com>
2013-04-29 18:23:06 +0200

    Corrected the MsgEncoder merge error.

--- caddy_usbl/src/MsgEncoder.cpp
+++ caddy_usbl/src/MsgEncoder.cpp
@@ -47,6 +47,8 @@
 #include <bitset>
 #include <iostream>
 #include <boost/integer/integer_mask.hpp>
+#include <cassert>
+#include <boost/regex.hpp>
 
 #define ADD_DIVER_MESSAGE(NAME, CODE, DEPTHSIZE, \
 	LATLONSIZE, MSGSIZE) \
@@ -59,21 +61,66 @@
 
 struct DiverMsg
 {
+	ADD_DIVER_MESSAGE(PositionInit,1,0,22,0);
+	ADD_DIVER_MESSAGE(Position,2,7,18,1);
+	ADD_DIVER_MESSAGE(PositionMsg,3,7,7,23);
+	ADD_DIVER_MESSAGE(PositionDef,5,7,14,9);
+
+	DiverMsg():
+		latitude(0),
+		longitude(0),
+		z(0),
+		depthRes(0.5){};
+
+	//\todo Add automatic extraction of lat-lon data from double values
+	template <class msg>
 	uint64_t pack()
 	{
-		uint64_t result(type);
-		result <<= depthSize;
-		result |= depth & boost::low_bits_mask_t<depthSize>::sig_bits;
-		result <<= latlonSize;
-		result |= lat & boost::low_bits_mask_t<latlonSize>::sig_bits;
-		result <<= latlonSize;
-		result |= lon & boost::low_bits_mask_t<latlonSize>::sig_bits;
-		result <<= msgSize;
-		result |= payload & boost::low_bits_mask_t<msgSize>::sig_bits;
-		return result;
+		fullmsg = msg::type;
+		fullmsg <<= msg::depthSize;
+		fullmsg |= int(z/depthRes) & boost::low_bits_mask_t<msg::depthSize>::sig_bits;
+		fullmsg <<= msg::latlonSize;
+		latlonToBits<msg::latlonSize>(latitude,longitude);
+		fullmsg |= lat & boost::low_bits_mask_t<msg::latlonSize>::sig_bits;
+		fullmsg <<= msg::latlonSize;
+		fullmsg |= lon & boost::low_bits_mask_t<msg::latlonSize>::sig_bits;
+		fullmsg <<= msg::msgSize;
+		fullmsg |= this->msg & boost::low_bits_mask_t<msg::msgSize>::sig_bits;
+		return fullmsg;
 	}
 
-	uint64_t depth, lat, lon, payload;
+	static inline uint8_t testType(uint64_t data, size_t msgSize = 48)
+	{
+		return (data >> (msgSize - 4)) & 0xF;
+	}
+
+	template <class msg>
+	bool unpack(uint64_t data)
+	{
+		fullmsg=data;
+		this->msg = data & boost::low_bits_mask_t<msg::msgSize>::sig_bits;
+		data >>= msg::msgSize;
+		lon = data & boost::low_bits_mask_t<msg::latlonSize>::sig_bits;
+		data >>= msg::latlonSize;
+		lat = data & boost::low_bits_mask_t<msg::latlonSize>::sig_bits;
+		data >>= msg::latlonSize;
+		depth = data & boost::low_bits_mask_t<msg::depthSize>::sig_bits;
+		data >>= msg::depthSize;
+
+		assert(((data & 0x0F) == msg::type) &&
+				"DiverMsg desired unpack type and received data type do not match.");
+	};
+
+	template <size_t precission>
+	std::pair<int,int> latlonToBits(double lat, double lon){return std::pair<int,int>();};
+
+	double latitude, longitude, z, depthRes;
+	uint64_t fullmsg;
+	uint8_t depth;
+	int lat,lon;
+	uint64_t msg;
+	//uint8_t noKML, def, checksum;
+	//int kmlX, kmlY;
 };
 
 template <>
@@ -250,8 +297,28 @@ int main(int argc, char* argv[])
 	}*/
 	//18 bit
 
-	std::bitset<48> result(msg.pack());
-	std::cout<<"Result:"<<result<<std::endl;
+	usleep(5000*1000);
+
+	step = 3./60;
+	for (int i=0; i<4; ++i)
+	{
+		msg.latitude += step;
+		msg.longitude += step;
+		msg.pack<DiverMsg::Position>();
+		writeLatLon(msg);
+		sendToModem(port, msg);
+		usleep(1000*1000);
+	}
+	step = 3./60;
+	for (int i=0; i<8; ++i)
+	{
+		msg.latitude -= step;
+		msg.longitude -= step;
+		msg.pack<DiverMsg::Position>();
+		writeLatLon(msg);
+		sendToModem(port, msg);
+		usleep(1000*1000);
+	}
 
 	return 0;
 }
