522a5f1c11e064fecd970bce33081b17e4c5fd95
Đula Nađ <dula.nad@gmail.com>
2013-05-13 16:10:34 +0200

    Added the VideoRay option.

--- cart2/launch/jarun/simenv.bash
+++ cart2/launch/jarun/simenv.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 export ROBOT=sim
 export LOCATION=jarun
-export JOYSTICK=/dev/input/js0
+export JOYSTICK=/dev/input/js1
 export PACKAGE=cart2
 export USE_UWSIM=0
 export USE_RVIZ=1
@@ -9,4 +9,6 @@ export IS_SIM=1
 export IS_REMOTE=0
 export NO_NOISE=0
 export LOGDIR=.
-export ENABLE_LOGGING=1
+export ENABLE_LOGGING=0
+export USE_VR=0
+export USE_CART=0
