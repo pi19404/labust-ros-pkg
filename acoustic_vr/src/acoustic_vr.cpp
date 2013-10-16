0a05fbcf9249200c7178db6490cfeaf656bd938f
avasilijevic <antonio.vasilijevic@fer.hr>
2013-10-16 10:19:30 +0200

    Update acoustic_vr.cpp
    Update of WPs on every target position change

--- acoustic_vr/src/acoustic_vr.cpp
+++ acoustic_vr/src/acoustic_vr.cpp
@@ -109,6 +109,7 @@ namespace FMOD
 	// varijable koje se upisu "Rucno"
 	double AltitudeSetPoint = 10.0f;
 	bool TrajectoryTracking = false;
+	bool WPupdateFlage=true;
 	double ObjectLat = 43.5;
 	double ObjectLong = 15.5;
 	double ObjectDepth = 20.0f;
@@ -1106,6 +1107,8 @@ namespace FMOD
 		//Ovdje ide iz simulatora dodavanje
 		///////////////////////////////////////////////////////////////////////////////////////////////////
 		boost::mutex::scoped_lock lock(joyMux);
+		if ((ObjectLong != target.position.north) || (ObjectLat != target.position.east))
+		{WPupdateFlage=true;}
 		ObjectLong = target.position.north;// + RabbitDistance*cos(target.orientation.yaw);
 		ObjectLat = target.position.east;// + RabbitDistance*sin(target.orientation.yaw);
 		ObjectDepth = target.position.depth;
@@ -1120,10 +1123,14 @@ namespace FMOD
 		}
 		
 		NumberOfWP = 2;
-		WPnorth[1] = zListenerPos - fabs(zListenerPos)/(zListenerPos)*5;
-		WPeast[1] = xListenerPos - fabs(xListenerPos)/(xListenerPos)*5;
-		WPnorth[2] = 0;//ObjectLong;
-		WPeast[2] = 0;//ObjectLat;
+		if (WPupdateFlage)
+		{
+			WPnorth[1] = zListenerPos - fabs(zListenerPos)/(zListenerPos)*5;
+			WPeast[1] = xListenerPos - fabs(xListenerPos)/(xListenerPos)*5;
+			WPnorth[2] = 0;//ObjectLong;
+			WPeast[2] = 0;//ObjectLat;
+			WPupdateFlage=false;
+		}
 		
 		zRotation = state.orientation.roll; //rovStatus["Roll"]; //sve je izmjesano z-naprijed, x-lateral, y-gore/dolje
 		xRotation = state.orientation.pitch; //rovStatus["Pitch"];
