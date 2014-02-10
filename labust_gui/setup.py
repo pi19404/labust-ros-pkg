9d9cc8c3bef1fe2af5ce1836f0b18b4051f9811c
Đula Nađ <dula.nad@gmail.com>
2014-02-10 23:17:31 +0100

    Update setup.py

--- labust_gui/setup.py
+++ labust_gui/setup.py
@@ -3,7 +3,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['labust_gui'],
+    packages=['labust_rqt','control'],
     package_dir={'': 'src'},
     requires=['std_msgs', 'rospy', 'rqt_gui', 'rqt_gui_py'])
 
