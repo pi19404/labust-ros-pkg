cd7aac2556a113f8992a5882c57438bc0cf4bb1e
Đula Nađ <dula.nad@gmail.com>
2014-02-11 15:43:40 +0100

    Exported the cart2, cmre-aidnav, auditory display packages in their individual repositories.

--- aided_navigation/matlab/estimation/ekf_init.m
+++
@@ -1,9 +0,0 @@
-function ekf = ekf_init(x0, P0, Ts, ekf_f,ekf_h)
-    ekf.xk = x0;
-    ekf.xk_1 = x0;
-    ekf.P = P0;
-    ekf.Pk_1 = P0;
-    ekf.Ts = Ts;
-    ekf.f = ekf_f;
-    ekf.h = ekf_h;
-end
--- aided_navigation/matlab/estimation/pv_model_h.m
+++
@@ -1,7 +0,0 @@
-function [yk,Hk,Vk,Rk] = pv_model_h(xk, Ts, yk_meas)
-    H = [1 0];
-    yk = H*xk;
-    Hk = H;
-    Vk = [1 0];
-    Rk = 0.056;
-end
