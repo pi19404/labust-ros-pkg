ea111cb639c4fe5fbd4eacf3365298848b235766
Đula Nađ <dula.nad@gmail.com>
2014-03-15 11:47:00 +0100

    Removed all the ported packages from the current tree.

--- caddy/caddy_uwsim/launch/do_batch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cpp -E -P -DPP_INNER_LOOP_W=${1} -DPP_OUTER_LOOP_W=${2} simulation_batch.yaml.meta > simulation_batch.yaml
-cpp -E -P -D PP_LOG_FILE="\"/home/dnad/Development/Matlab/ros/pladypos_log_ideal_${1}_${2}.csv\"" simulation_batch.launch.meta > simulation_batch.launch
