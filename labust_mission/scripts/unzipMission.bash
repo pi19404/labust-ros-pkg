e77444fa4e5836d487d1b28b3b3116fa69ec48e6
gibo1990 <filip.mandic@gmail.com>
2014-05-15 10:32:59 +0200

    Removed relative paths

--- labust_mission/scripts/unzipMission.bash
+++ labust_mission/scripts/unzipMission.bash
@@ -2,6 +2,6 @@
 
 echo "Unzipping mission archive... "
 echo $1
-
-unzip -o $1 -d /home/filip/catkin_ws/src/labust-ros-pkg/labust_mission/data/extracted/ 
+mkdir -p `rospack find labust_mission`/data/extracted/
+unzip -o $1 -d `rospack find labust_mission`/data/extracted/ 
 echo "Archive unzipped."
